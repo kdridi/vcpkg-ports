723023e8ce9a45b66d90e87bf78cebfda77419c4
Karim DRIDI <karim.dridi@eliis.fr>
2026-01-23 14:22:29 +0100

    ci: Update portfile and CMake configuration for FESAPI integration

--- fesapi/portfile.cmake
+++ fesapi/portfile.cmake
@@ -1,51 +1,30 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO F2I-Consulting/fesapi
-    REF "v${VERSION}"
-    SHA512 83823e3ce3acfc1428f5d0fa8fd96dd49befce3e2f8827b4894403b215f9ef117bcd55af981c33fc019cc5bfeac76c62265fc052285307fbabe2630d91bba14d
-    HEAD_REF fesapi
-    PATCHES
-        fix-configure-file.patch
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO F2I-Consulting/fesapi
+	REF v2.13.0.0
+	SHA512 1ccbcef17f484f1aebbfff0347fd4aaf51d86d9389645cf2059cc6d47d69863c9bde368df438ac7d9146c6dd111d1da6bac1910146977629ff0a7cddb5848a08
+	HEAD_REF master
+	PATCHES
+		eliis.patch
 )
 
-# Pre-create version_config.h to avoid parallel build issues
-file(READ "${SOURCE_PATH}/cmake/version_config.h.in" VERSION_CONFIG_IN)
-string(REPLACE "\${Fesapi_VERSION_MAJOR}" "2" VERSION_CONFIG_IN "${VERSION_CONFIG_IN}")
-string(REPLACE "\${Fesapi_VERSION_MINOR}" "14" VERSION_CONFIG_IN "${VERSION_CONFIG_IN}")
-string(REPLACE "\${Fesapi_VERSION_PATCH}" "0" VERSION_CONFIG_IN "${VERSION_CONFIG_IN}")
-string(REPLACE "\${Fesapi_VERSION_TWEAK}" "0" VERSION_CONFIG_IN "${VERSION_CONFIG_IN}")
-string(REPLACE "\${Fesapi_VERSION}" "2.14.0.0" VERSION_CONFIG_IN "${VERSION_CONFIG_IN}")
-file(WRITE "${SOURCE_PATH}/src/version_config.h" "${VERSION_CONFIG_IN}")
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+	set(FESAPI_STATIC_CMAKE_OPTIONS -DHDF5_USE_STATIC_LIBRARIES=ON -DZLIB_USE_STATIC_LIBS=ON)
+endif ()
 
-# Pre-create HidtType.h for HDF5 >= 1.10 (default case)
-file(READ "${SOURCE_PATH}/cmake/HidtType.h.in" HIDTTYPE_IN)
-string(REPLACE "\${COMMENT_HDF5_1_8}" "//" HIDTTYPE_IN "${HIDTTYPE_IN}")
-string(REPLACE "\${COMMENT_HDF5_1_10}" "" HIDTTYPE_IN "${HIDTTYPE_IN}")
-file(WRITE "${SOURCE_PATH}/src/common/HidtType.h" "${HIDTTYPE_IN}")
-
-# Pre-create nsDefinitions.h with default namespace values
-file(READ "${SOURCE_PATH}/cmake/nsDefinitions.h" NS_DEFINITIONS_IN)
-string(REPLACE "\${FESAPI_COMMON_NS}" "common" NS_DEFINITIONS_IN "${NS_DEFINITIONS_IN}")
-string(REPLACE "\${FESAPI_RESQML2_NS}" "resqml2" NS_DEFINITIONS_IN "${NS_DEFINITIONS_IN}")
-string(REPLACE "\${FESAPI_RESQML2_0_1_NS}" "resqml2_0_1" NS_DEFINITIONS_IN "${NS_DEFINITIONS_IN}")
-string(REPLACE "\${FESAPI_RESQML2_2_NS}" "resqml2_2" NS_DEFINITIONS_IN "${NS_DEFINITIONS_IN}")
-string(REPLACE "\${FESAPI_WITSML2_NS}" "witsml2" NS_DEFINITIONS_IN "${NS_DEFINITIONS_IN}")
-string(REPLACE "\${FESAPI_WITSML2_1_NS}" "witsml2_1" NS_DEFINITIONS_IN "${NS_DEFINITIONS_IN}")
-string(REPLACE "\${FESAPI_PRODML2_3_NS}" "prodml2_3" NS_DEFINITIONS_IN "${NS_DEFINITIONS_IN}")
-string(REPLACE "\${FESAPI_EML2_NS}" "eml2" NS_DEFINITIONS_IN "${NS_DEFINITIONS_IN}")
-string(REPLACE "\${FESAPI_EML2_0_NS}" "eml2_0" NS_DEFINITIONS_IN "${NS_DEFINITIONS_IN}")
-string(REPLACE "\${FESAPI_EML2_3_NS}" "eml2_3" NS_DEFINITIONS_IN "${NS_DEFINITIONS_IN}")
-file(WRITE "${SOURCE_PATH}/src/nsDefinitions.h" "${NS_DEFINITIONS_IN}")
-
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
+vcpkg_configure_cmake(
+	SOURCE_PATH "${SOURCE_PATH}"
+	DISABLE_PARALLEL_CONFIGURE
+	OPTIONS ${FEATURE_OPTIONS} ${FESAPI_STATIC_CMAKE_OPTIONS}
 )
 
-vcpkg_cmake_install()
+vcpkg_install_cmake()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_fixup_cmake_targets(
+	CONFIG_PATH lib/cmake/FesapiCpp
+)
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_install_copyright(
+	FILE_LIST
+	"${SOURCE_PATH}/LICENSE"
+)
